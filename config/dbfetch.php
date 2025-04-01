43e715ffed77e3ed3f623b7ae0154947c3f254d7
Mark Simon <msimonbringas05v2@gmail.com>
2025-04-01 21:43:31 +0800

    Edit: Modularized data fetch and session handling

---
+++ config/dbfetch.php
@@ -0,0 +1,15 @@
+<?php 
+    include '../config/dbconfig.php';
+    include '../config/session.php';
+
+    // fetches access control
+    $accessQuery = "SELECT access_level FROM employee_details WHERE emp_id = :emp_id";
+    $access = $pdo->prepare($accessQuery);
+    $access->execute([":emp_id" => $_SESSION['emp_id']]);
+    $accessLevel = $access->fetchColumn();
+
+    // fetches employee details
+    $empQuery = "SELECT * FROM employee_details WHERE emp_id = :emp_id";
+    $empDetails = $pdo->prepare($empQuery);
+    $empDetails->execute([":emp_id" => $_SESSION['emp_id']]);
+?>
