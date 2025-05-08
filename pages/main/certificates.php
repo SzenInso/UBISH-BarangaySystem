3ff789c00057071da02903907bb46b83fc7f8b6e
jdgmn <20140976@s.ubaguio.edu>
2025-05-08 13:51:31 +0800

    FIX: fixed issue with sidebar not reflecting and redirecting properly

--- pages/main/certificates.php
+++ pages/main/certificates.php
@@ -170,7 +170,7 @@ if (isset($_POST['submit_certificate'])) {
                         echo '<li><a href="../main/permits.php">Permit Requests</a></li>';
                     } ?>
                     <h3>Reports</h3>
-                    <li><a href="#">Incident Reports</a></li>
+                    <li><a href="../main/incidents.php">Incident Reports</a></li>
                     <li><a href="../main/reports.php">Analytics</a></li>
                 </ul>
             </div>
