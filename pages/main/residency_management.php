35b06263fbe7cfac2a4f019bb7af9a1afec4522c
Mark Simon <msimonbringas05v2@gmail.com>
2025-05-30 22:06:59 +0800

    Edit: Title tag

--- pages/main/residency_management.php
+++ pages/main/residency_management.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/style.css">
-    <title>UBISH Dashboard | Home</title>
+    <title>UBISH Dashboard | Residency Management</title>
 </head>
 <body>
     <style>
